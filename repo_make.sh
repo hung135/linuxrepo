3d1d61512737226822c6843cb1b40bf9ef57b50f
root <root@7bd4db20e160>
2019-11-23 09:46:11 +0000

    added another repo

--- repo_make.sh
+++ repo_make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #reposync -n -r docker-ce-stable -p ./repos
+#make sure you have repost config ceated /etc/yum.repos.d/yourepos.repo
 dnf reposync -p repos --repo docker-ce-stable --newest-only
 createrepo repos/docker-ce-stable
-
+dnf reposync -p repos --repo centos_7 
+createrepo repos/centos_7
 docker build -t taurus/linuxrepo .
