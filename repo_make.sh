762188b9440062be99d1454b704311f293a27ad8
Hung Nguyen <hung135@hotmail.com>
2019-11-23 04:58:30 -0500

    adding sample repofile

--- repo_make.sh
+++ repo_make.sh
@@ -3,6 +3,8 @@
 #make sure you have repost config ceated /etc/yum.repos.d/yourepos.repo
 dnf reposync -p repos --repo docker-ce-stable --newest-only
 createrepo repos/docker-ce-stable
-dnf reposync -p repos --repo centos_7 
+dnf reposync -p repos --repo centos_7 --newest-only
 createrepo repos/centos_7
+dnf reposync -p repos --repo centos_7_dotnet --newest-only
+createrepo repos/centos_7_dotnet
 docker build -t taurus/linuxrepo .
